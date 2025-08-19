1af3e77baca12af242f6fa5f945b5b9d38cb61f1
Human Resource <hr@irinfotech.com>
2025-08-19 18:09:54 +0000

    in the verify certificate page we are having some issues;
    so the resolu

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -15,6 +16,12 @@ const nextConfig: NextConfig = {
         hostname: 'placehold.co',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/uploads/**',
       },
     ],
   },
